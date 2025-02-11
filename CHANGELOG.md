e9b99c6a6e8111f9a0b3b9d334f4f5f7c35d5594
Thibaud Colas <thibaudcolas@gmail.com>
2025-02-11 22:12:00 +0000

    Release v1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@
 > All notable changes to this project will be documented in this file.
 > This project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [v1.0.0](https://github.com/thibaudcolas/markov_draftjs/releases/tag/v1.0.0)
+
+This release is identical to the previous v0.2.0, but is a major version bump to indicate that the package is now considered stable.
+
 ## [v0.2.0](https://github.com/thibaudcolas/markov_draftjs/releases/tag/v0.2.0)
 
 ### Changed
--- markov_draftjs/__init__.py
+++ markov_draftjs/__init__.py
@@ -1,19 +1,21 @@
 import json
 import os
 
-__title__ = 'markov_draftjs'
-__version__ = '0.2.0'
-__description__ = 'Draft.js sample content generated with Markov chains of Project Gutenberg books.'
-__url__ = 'https://github.com/thibaudcolas/markov_draftjs'
-__author__ = 'Thibaud Colas'
-__author_email__ = 'thibaudcolas@gmail.com'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2017-present %s' % __author__
+__title__ = "markov_draftjs"
+__version__ = "1.0.0"
+__description__ = (
+    "Draft.js sample content generated with Markov chains of Project Gutenberg books."
+)
+__url__ = "https://github.com/thibaudcolas/markov_draftjs"
+__author__ = "Thibaud Colas"
+__author_email__ = "thibaudcolas@gmail.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2017-present %s" % __author__
 
 
 def get_content_sample():
-    content_path = os.path.join(os.path.dirname(__file__), 'content.json')
-    with open(content_path, 'r') as f:
+    content_path = os.path.join(os.path.dirname(__file__), "content.json")
+    with open(content_path, "r") as f:
         content = json.loads(f.read())
 
     return content
